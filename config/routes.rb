b456c8ef302e1fa83dc051a9a33490dfa307785f
minhpham <minhpham161895@gmail.com>
2022-03-24 17:35:20 +0700

    Finish Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root    'static_pages#home'
+  root              'static_pages#home'
 
-  get 'home' => 'static_pages#home'
-  get 'contact' => 'static_pages#contact'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
+  get 'home'    =>  'static_pages#home'
+  get 'contact' =>  'static_pages#contact'
+  get 'help'    =>  'static_pages#help'
+  get 'about'   =>  'static_pages#about'
+  get 'signup'  =>  'users#new'
+  resources :users
 end
