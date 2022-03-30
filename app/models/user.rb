483c7a8a88d61ebd24df2ce8d88872a1ca956cd8
minhpham <minhpham161895@gmail.com>
2022-03-31 02:06:51 +0700

    Finish following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -75,7 +75,10 @@ class User < ActiveRecord::Base
     end
 
     def feed
-        Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                            WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+        following_ids: following_ids, user_id: id)
     end
 
     def follow(other_user)
@@ -84,6 +87,10 @@ class User < ActiveRecord::Base
 
     def unfollow(other_user)
         following.delete(other_user)
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
     end
 
     private
